c4304619579a25db4775b621eed1a1c04ca5e5a8
yskl7137 <yskl7137@gmail.com>
2021-12-21 15:27:13 +0900

    Delete Bug Fix (List Cell Optional Error)

--- RlfxoTimeStamper/ListCell.swift
+++ RlfxoTimeStamper/ListCell.swift
@@ -16,9 +16,11 @@ struct ListCell: View {
     var body: some View {
         VStack(alignment: .leading) {
             HStack() {
-                Text("\(times.name!)\n\(times.insertTime!,formatter: self.DateForm) ~ \(times.saveTime!,formatter: self.RelativeForm)")
+                if let name = times.name {
+                Text("\(name)\n\(times.insertTime!,formatter: self.DateForm) ~ \(times.saveTime!,formatter: self.RelativeForm)")
                     .lineLimit(3)
                     .multilineTextAlignment(.leading)
+                }
                 Spacer()
                 Text("\((times.time)/3600)시간 \((times.time)%3600/60)분 \((times.time)%60)초")
                     .fontWeight(.medium)
